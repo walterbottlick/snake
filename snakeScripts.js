507acdd49bec0ea63221239372c8f3adaf1d7f5d
Walter <walter.bottlick@umuc.edu>
2017-03-16 23:48:16 -0400

    commit

--- snakeScripts.js
+++ snakeScripts.js
@@ -7,6 +7,10 @@ var EMPTY=0, SNAKE=1, FRUIT=2;
 // Directions
 var LEFT = 0, UP = 1, RIGHT = 2, DOWN = 3;
 
+// KeyCodes
+var KEY_LEFT = 37, KEY_UP = 38, KEY_RIGHT = 39, KEY_DOWN = 40;
+
+
 
 var grid = {
 	
@@ -77,7 +81,7 @@ function setFood() {
 
 
 // Game Objects
-var canvas, ctx, keystate, frames;
+var canvas, ctx, keystate, frames, score;
 
 
 function main() {
@@ -87,14 +91,24 @@ function main() {
 	ctx = canvas.getContext("2d");
 	document.body.appendChild(canvas);
 
+	ctx.font = "12px Helvetica";
+
 	frames = 0;
 	keystate = {};
+	document.addEventListener("keydown", function(evt) {
+		keystate[evt.keyCode] = true;
+	});
+	document.addEventListener("keyup", function(evt) {
+		delete keystate[evt.keyCode];
+	});
 
 	init();
 	loop();
 }
 
 function init() {
+	score = 0;
+
 	grid.init(EMPTY, COLS, ROWS);
 
 	var sp = {x:Math.floor(COLS/2), y:ROWS-1};
@@ -113,6 +127,57 @@ function loop() {
 
 function update() {
 	frames++;
+
+	if (keystate[KEY_LEFT] && snake.direction !== RIGHT)
+		snake.direction = LEFT;
+	if (keystate[KEY_UP] && snake.direction !== DOWN)
+		snake.direction = UP;
+	if (keystate[KEY_RIGHT] && snake.direction !== LEFT)
+		snake.direction = RIGHT;
+	if (keystate[KEY_DOWN] && snake.direction !== UP)
+		snake.direction = DOWN;
+
+	if (frames % 5 === 0) {
+		var nx = snake.last.x;
+		var ny = snake.last.y;
+
+		switch (snake.direction) {
+			case LEFT:
+				nx--;
+				break;
+			case UP:
+				ny--;
+				break;
+			case RIGHT:
+				nx++;
+				break;
+			case DOWN:
+				ny++;
+				break;
+		}
+
+		if (0 > nx || nx > grid.width - 1 ||
+			0 > ny || ny > grid.height - 1 ||
+			grid.get(nx, ny) === SNAKE) {
+			return init();
+		}
+
+		if (grid.get(nx, ny) === FRUIT) {
+			var tail = {x:nx, y:ny};
+			score++;
+			setFood();
+		} else {
+			var tail = snake.remove();
+			grid.set(EMPTY, tail.x, tail.y);
+			tail.x = nx;
+			tail.y = ny;
+		}
+
+		
+		grid.set(SNAKE, tail.x, tail.y);
+
+		snake.insert(tail.x, tail.y);
+	}
 }
 
 function draw() {
@@ -123,10 +188,10 @@ function draw() {
 		for (var y = 0; y < grid.height; y++) {
 			switch (grid.get(x, y)) {
 				case EMPTY:
-					ctx.fillStyle = "#fff";
+					ctx.fillStyle = "#000";
 					break;
 				case SNAKE:
-					ctx.fillStyle = "#0ff";
+					ctx.fillStyle = "#fff";
 					break;
 				case FRUIT:
 					ctx.fillStyle = "#f00";
@@ -135,6 +200,9 @@ function draw() {
 			ctx.fillRect(x * tw, y * th, tw, th);
 		}
 	}
+
+	ctx.fillStyle = "#fff";
+	ctx.fillText("SCORE: " + score, 10, canvas.height - 10);
 }
 
 main();
